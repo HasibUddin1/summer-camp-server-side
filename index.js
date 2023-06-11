ca519f8a95e8aace80cc47926b1668bcdeba13b4
IconicLuck <uhasib959@gmail.com>
2023-06-11 15:34:11 +0600

    created patch link to make a user admin or instructor

--- index.js
+++ index.js
@@ -140,6 +140,36 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id
+
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                }
+            }
+
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        // user apis end
+
         app.get('/selectedClasses/:email', async (req, res) => {
             const email = req.params.email
             const query = { email: email }
